8dcb15aa4541c128869fa9ae7aad67808ec40cf4
Thomas <147412616+ColettesCorvette@users.noreply.github.com>
2025-11-20 16:58:24 +0100

    configuration du serveur

--- serveur.c
+++ serveur.c
@@ -9,11 +9,27 @@
 
 
 int main() {
+
+	int socket_fd = 0;
+
+	//initialisation du socket pour une communcation IPv4 utilisant le protocol TCP
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Failed to create the socket\n");
+		exit(EXIT_FAILURE);
+	}
 	
-	//initialisation du socket pour un communcation IPV4 utilisant le protocol TCP
-	int socket = socket(AF_INET, SOCK_STREAM, 0);
-	
-	printf("Valeur de retour du socket%d", socket);
+	//création de l'adresse et du port du serveur 
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET; //Format IPv4
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //Adresse IP
+	serv_addr.sin_port = htons(11000); // Port TCP
+
+	//Associe l'adresse au socket
+	if (bind(socket_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		perror("Failed to bind the address to the socket server \n");
+		exit(EXIT_FAILURE);	
+	}
 	
 	
 	
@@ -22,4 +38,3 @@ int main() {
 
 
 
-//gcc -Wall -Wextra -Werror serveur.c

6cfd6ac8576381003e2e50176f6c2cad32a7f321
Thomas <147412616+ColettesCorvette@users.noreply.github.com>
2025-11-23 15:38:19 +0100

    Renommage create_socket en create_valid_socket_tcp

--- network_utils.h
+++ network_utils.h
@@ -1,7 +1,7 @@
 #ifndef NETWORK_UTILS_H
 #define NETWORK_UTILS_H
 
-int create_socket(bool reuse, bool nodeplay);
+int create_valid_socket_tcp(bool reuse, bool nodeplay);
 
 //static void print_error(const char *msg);
 
